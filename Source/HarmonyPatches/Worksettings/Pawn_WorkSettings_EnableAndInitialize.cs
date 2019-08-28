bb32775f56e41d31d41026775c85bfc32a38ecc8
thghca <thghca@gmail.com>
2019-08-28 20:40:48 +0500

    Clean favourite in case of pawn reccurect to avoid overriding

---
+++ Source/HarmonyPatches/Worksettings/Pawn_WorkSettings_EnableAndInitialize.cs
@@ -0,0 +1,21 @@
+﻿// Karel Kroeze
+// Pawn_WorkSettings_SetPriority.cs
+// 2017-05-22
+
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace WorkTab
+{
+    [HarmonyPatch( typeof( Pawn_WorkSettings), "EnableAndInitialize")]
+    public class Pawn_WorkSettings_EnableAndInitialize
+    {
+        static void Prefix(Pawn_WorkSettings __instance)
+        {
+            //clean favourite in case of pawn reccurect to avoid overriding
+            var pawn = __instance.Pawn();
+            FavouriteManager.Get[pawn] = null;
+        }
+    }
+}
